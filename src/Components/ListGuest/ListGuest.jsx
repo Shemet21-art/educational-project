828f646f5f0a6f4437e2029a75dc8459ef58687b
unknown <shemetttttttt8@gmail.com>
2023-12-16 15:44:09 +0100

    add : modal

--- src/Components/ListGuest/ListGuest.jsx
+++ src/Components/ListGuest/ListGuest.jsx
@@ -1,6 +1,22 @@
 import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useEffect, useState } from "react";
 import UniqInput from "../UniqInput";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+import Typography from '@mui/material/Typography';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 function guestInfo( name, surname){
    return {name,surname}
@@ -19,12 +35,18 @@ function ListGuest(){
 const [nameValue, setNameValue] = useState('')
 const [surnameValue, setSurnameValue] = useState('')
 
+const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
 function addEvent(){
     if( nameValue !== ''&& surnameValue !==''){
   listNamesGuests.push(guestInfo( nameValue, surnameValue)) 
   setNameValue('')
   setSurnameValue('')}
   else alert("Заполни Все поля!")
+  handleClose()
   
 }
     return(<>
@@ -47,7 +69,16 @@ function addEvent(){
         </Table>
     </TableContainer>
     </div>
-<form>
+
+<Button onClick={handleOpen}> add guest</Button>
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+  <form>
    <UniqInput 
    label={"Введите имя гостя"}
    value = {nameValue}
@@ -59,7 +90,9 @@ function addEvent(){
   onChange={(e)=>setSurnameValue(e.target.value)}
    />
     <Button onClick={addEvent}>Add</Button>
-    </form>
+    </form>  </Box>
+</Modal>
+
     </>)
 }
 
